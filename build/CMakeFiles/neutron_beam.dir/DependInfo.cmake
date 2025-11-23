3d6300f1353f7084c6b886f9ae3c570316bdfcae
jackiekuroda <jk947921@ohio.edu>
2025-11-23 12:41:23 -0500

    removed neutron produced and detected counters as they were not needed

---
+++ build/CMakeFiles/neutron_beam.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/storage/Jackie/Au_activation_test/neutron_beam.cc" "CMakeFiles/neutron_beam.dir/neutron_beam.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/neutron_beam.cc.o.d"
+  "/storage/Jackie/Au_activation_test/src/ActionInitialization.cc" "CMakeFiles/neutron_beam.dir/src/ActionInitialization.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/src/ActionInitialization.cc.o.d"
+  "/storage/Jackie/Au_activation_test/src/DetectorConstruction.cc" "CMakeFiles/neutron_beam.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/src/DetectorConstruction.cc.o.d"
+  "/storage/Jackie/Au_activation_test/src/PhysicsList.cc" "CMakeFiles/neutron_beam.dir/src/PhysicsList.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/src/PhysicsList.cc.o.d"
+  "/storage/Jackie/Au_activation_test/src/PrimaryGeneratorAction.cc" "CMakeFiles/neutron_beam.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/storage/Jackie/Au_activation_test/src/RunAction.cc" "CMakeFiles/neutron_beam.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/src/RunAction.cc.o.d"
+  "/storage/Jackie/Au_activation_test/src/SteppingAction.cc" "CMakeFiles/neutron_beam.dir/src/SteppingAction.cc.o" "gcc" "CMakeFiles/neutron_beam.dir/src/SteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/neutron_beam.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/neutron_beam.dir/neutron_beam.cc.o"
+  "CMakeFiles/neutron_beam.dir/neutron_beam.cc.o.d"
+  "CMakeFiles/neutron_beam.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/neutron_beam.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/neutron_beam.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/neutron_beam.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/neutron_beam.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/neutron_beam.dir/src/PhysicsList.cc.o.d"
+  "CMakeFiles/neutron_beam.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/neutron_beam.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/neutron_beam.dir/src/RunAction.cc.o"
+  "CMakeFiles/neutron_beam.dir/src/RunAction.cc.o.d"
+  "CMakeFiles/neutron_beam.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/neutron_beam.dir/src/SteppingAction.cc.o.d"
+  "neutron_beam"
+  "neutron_beam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/neutron_beam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
