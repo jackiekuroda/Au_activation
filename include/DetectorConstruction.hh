1c74fc869c89bc2af14c8f950e34bc568c5ce567
Jackie <160259224+jackiekuroda@users.noreply.github.com>
2026-03-01 17:02:15 -0500

    added the detector to the simulation

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -24,9 +24,65 @@ public:
 
     virtual G4VPhysicalVolume* Construct();
 
+    DetectorConstruction(G4double outerDeadLayerThick);
+    virtual ~DetectorConstruction();
+
+    G4LogicalVolume* GetLogicDetector() const { return fLogActiveCrystal; }
+
+    virtual G4VPhysicalVolume* Construct();
+    virtual void ConstructSDandField();
+
+
     // Material and geometry construction helpers
     void SetSource(G4bool value);
-    void UpdateGeometry();
+
+    void SetOutDeadLayerThickness(double value);
+    void SetCoverThick(G4double value);
+    void SetShellRadius(G4double value);
+    void SetShellLength(G4double value);
+    void SetShellThick(G4double value);
+    void SetEndGap(G4double value);
+    void SetholderLength(G4double value);
+   void SetholderThick(G4double value);
+   void SetholderTopThick(G4double value);
+   void SetholderBottomThick(G4double value);
+   void SetmylarThick (G4double value);
+   void SetkaptonThick (G4double value);
+   void SetCrystalRadius (G4double value);
+   void SetCrystalHalfLength (G4double value);
+   void SetCrystalEndRadius (G4double value);
+   void SetHoleDepth(G4double value);
+   void SetHoleRadius(G4double value);
+   void SetOuterDeadLayerThick(G4double value);
+   void SetInnerDeadLayerThick(G4double value);
+   void UpdateGeometry();
+   void SetSourceMove(G4double value);
+
+   void SetDetectorLength(G4double length);
+   void SetDetectorThickness(G4double thickness);
+   void SetDetectorMaterial(const G4String& name);
+
+   G4double GetouterDeadLayerThick() const {return fouterDeadLayerThick;};
+
+   G4double GetinnerDeadLayerThick() const {return innerDeadLayerThick;}
+
+   G4double GetCrystalRadius() const {return crystalRadius;}
+   G4double GetCrystalHalfLength() const {return crystalHalfLength;}
+
+   G4double GetShellRadius() const {return shellRadius;}
+   G4double GetShellLength() const {return shellLength;}
+   G4double GetShellThick() const {return shellThick;}
+
+   G4double GetholderLength() const {return holderLength;}
+   G4double GetholderThick() const {return holderThick;}
+   G4double GetholderTopThick() const {return holderTopThick;}
+   G4double GetholderBottomThick() const {return holderBottomThick;}
+
+   G4double GetkaptonThick() const {return kaptonThick;}
+   G4double GetmylarThick() const {return mylarThick;}
+
+   G4double GetHoleDepth() const {return holeDepth;}
+   G4double GetHoleRadius() const {return holeRadius;}
 
     G4double Gettargetthic() const;
 
@@ -35,16 +91,48 @@ private:
     void DefineMaterials();
     G4VPhysicalVolume* ConstructWorld();
 
+    void ConstructHPGeDetector(G4LogicalVolume* );
     void ConstructSource(G4LogicalVolume* motherLogicalVolume);
-
+    void ConstructPbShield(G4LogicalVolume* motherLogicalVolume);
+    void ConstructAustinLeadHouse(G4LogicalVolume* motherLogicalVolume);
+    void ConstructRoom(G4LogicalVolume* motherLogicalVolume);
+    G4LogicalVolume* logicDetector = nullptr;
     // Core geometry
     G4VPhysicalVolume* physiWorld;
 
-    //flags
+    // Detector dimensions
+    G4double coverThick;
+    G4double shellRadius;
+    G4double shellLength;
+    G4double shellThick;
+    G4double endGap;
+    G4double detectorMove;
+
+    G4double crystalRadius;
+    G4double crystalHalfLength;
+    G4double crystalEndRadius;
+    G4double holeDepth;
+    G4double holeRadius;
+    //G4double outerDeadLayerThick;
+    G4double fouterDeadLayerThick;
+    G4double innerDeadLayerThick;
+
+    //holder
+    G4double holderLength;
+    G4double holderThick;
+    G4double holderTopThick;
+    G4double holderBottomThick;
+    G4double mylarThick;
+    G4double kaptonThick;
+    G4double SourceMove;
+
+    // Flags
     G4bool fCheckOverlaps;
 
     // Materials
-    G4Material* Vacuum;
+    G4Material* GeCrystal;
+    G4Material* Shield_Air;
+    G4LogicalVolume* fLogActiveCrystal;
 
     // Optional
     G4UserLimits* stepLimit;
